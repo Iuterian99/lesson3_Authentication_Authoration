0fc6ef947fbe68199be72459245c407aaf65c994
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-15 01:14:13 +0500

    created

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*
+/:en/
+authentication is the process of verifying who someone is, whereas authorization is the process of verifying what specific applications, files, and data a user has access to.
+/:uz/
+! Authentication
+Saytda 1-marta Ro`yxatdan o`tayotgucha login qilishimiz Authentication deyiladi!
+!Authorization
+login qilgan odamni nimalarga huquqi borligini aniqlash
+!Authorizatsiyadan foydalanish uchun cmd yoki terminalda "npm i jsonwebtoken" deb ustanofka qilishimiz kk!
+ */
 const express = require('express');
 const app = express();
 const port = 9000;
