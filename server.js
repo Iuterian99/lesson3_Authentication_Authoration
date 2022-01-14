211bca69c993691e8be1333f138340a88bd13bc7
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-15 02:04:27 +0500

    added custom fs file

--- server.js
+++ server.js
@@ -6,18 +6,30 @@ authentication is the process of verifying who someone is, whereas authorization
 Saytda 1-marta Ro`yxatdan o`tayotgucha login qilishimiz Authentication deyiladi!
 !Authorization
 login qilgan odamni nimalarga huquqi borligini aniqlash
-!Authorizatsiyadan foydalanish uchun cmd yoki terminalda "npm i jsonwebtoken" deb ustanofka qilishimiz kk!
+! "npm i jsonwebtoken" buni 14-qatorda chaqirib oldim
+Authorizatsiyadan foydalanish uchun cmd yoki terminalda "npm i jsonwebtoken" deb ustanofka qilishimiz kk!
  */
 const express = require('express');
 const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+const customFs = require("./lib/fsDealFile");
+
+const users = new customFs("./model/users.json");
+console.log(users);
+
+
 const port = 9000;
 
-function middleware(req, res, next){
-console.log("Madina Rustamovna");
-next();
+app.post("/login", (req, res)=>{
+  const { name, password } = req.body;
+  const allUsers = users.read();
+  console.log(JSON.parse(allUsers));
+  res.send("Ma`lumot keldi!");
+  
+})
 
-}
-app.get("/", middleware, (req, res) =>{ 
+app.get("/posts", (req, res) =>{ 
   res.send("rahmat😍😍")
 })
 
